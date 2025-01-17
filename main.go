0c26f3efe87eeec3ea20628a366ddfc24630b039
SavingFrame <savingframe@gmail.com>
2025-01-17 19:10:54 +0100

    Remove some unused things\

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	stdio := server.NewStdio(nil, nil)
+	// TODO: Change path to local instead of absolute one
 	f, err := os.OpenFile("/Users/user/projects/snakelsp/log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
--- server/logger.go
+++ server/logger.go
@@ -1,13 +1,11 @@
 package server
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"io"
 	"log"
 	"log/slog"
-	"os"
 	"time"
 )
 
@@ -15,20 +13,6 @@ type JsonRpcLogger struct{}
 
 func (l *JsonRpcLogger) Printf(format string, v ...interface{}) {
 	log.Printf(format, v...)
-}
-
-func tempLog(s string) {
-	file, err := os.OpenFile("/Users/user/projects/snakelsp/output.txt", os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-		fmt.Println("File does not exists or cannot be created")
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	w := bufio.NewWriter(file)
-	fmt.Fprintln(w, s)
-
-	w.Flush()
 }
 
 type CustomTextHandler struct {
@@ -48,17 +32,12 @@ func (h *CustomTextHandler) Enabled(_ context.Context, level slog.Level) bool {
 }
 
 func (h *CustomTextHandler) Handle(_ context.Context, record slog.Record) error {
-	// Format the log message manually
 	timestamp := time.Now().Format(time.RFC3339) // Customize timestamp if needed
 	level := record.Level.String()               // Get the log level as string
 	message := record.Message                    // Default log message (no escaping)
 
-	// Print basic log message (no escaping)
 	fmt.Fprintf(h.writer, "time=%s level=%s msg=%s\n", timestamp, level, message)
-	// fmt.Fprintln(h.writer, message)
-	// fmt.Fprintln(h.writer, message)
 
-	// Print any additional attributes
 	record.Attrs(func(attr slog.Attr) bool {
 		// Special handling to ensure attributes with "\n" remain unescaped
 		if str, ok := attr.Value.Any().(string); ok && len(str) > 0 {
