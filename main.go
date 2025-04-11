5e66da5bc2ad5bdf791d9fc0238a444fd39b0b6d
SavingFrame <savingframe@gmail.com>
2025-04-11 23:55:15 +0200

    Fix path for logs

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"snakelsp/server"
 )
 
+type discardCloser struct {
+	io.Writer
+}
+
+func (d *discardCloser) Close() error {
+	return nil
+}
+
+func initializeLogs() (io.WriteCloser, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+	logFilePath := filepath.Join(homeDir, ".local", "state", "snakelsp", "snakelsp.log")
+	err = os.MkdirAll(filepath.Dir(logFilePath), os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+	f, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, err
+	}
+	return f, nil
+}
+
 func main() {
 	stdio := server.NewStdio(nil, nil)
-	// TODO: Change path to local instead of absolute one
-	f, err := os.OpenFile("/home/savingframe/Projects/snakelsp/log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := initializeLogs()
 	if err != nil {
-		log.Fatalf("error opening file: %v", err)
+		log.Printf("error initializing logs: %v", err)
+		f = &discardCloser{io.Discard}
 	}
 	defer f.Close()
 	srv := server.NewServer(f)
